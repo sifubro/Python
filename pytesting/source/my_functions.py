cc543cc9c92c95b807042d5fcdf29593ddcd6ff6
sifubro <theokasio@gmail.com>
2024-02-01 15:10:13 +0000

    Adding pytests with raise Errors

--- pytesting/source/my_functions.py
+++ pytesting/source/my_functions.py
@@ -3,3 +3,8 @@ def add(x,y):
 
 def divide(x,y):
     return x/y
+
+def divide_custom_raise(x,y):
+    if y == 0:
+        raise ValueError
+    return x / y
--- pytesting/tests/test_my_functions.py
+++ pytesting/tests/test_my_functions.py
@@ -1,7 +1,18 @@
 import pytest 
-from ..source.my_functions import add
+from ..source.my_functions import add, divide, divide_custom_raise
 
 
 def test_add():
     result = add(1,5)
     assert result == 6
+
+
+def test_divide_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(3,0)
+
+
+def test_divide_custom():
+    with pytest.raises(ValueError):
+        divide_custom_raise(3,0)
+
